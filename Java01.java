2698bc9fd04bd2c7c304736689cb0e6244c16c43
RosalesJhordi <1428060@senati.pe>
2023-08-17 09:30:29 -0500

    Crearcion de la clase principal

---
+++ Java01.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+
+public class Java01 {
+
+}
