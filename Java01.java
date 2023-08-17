6d00a118ceb3b69354cb3e5ae19164901d2204c9
RosalesJhordi <1428060@senati.pe>
2023-08-17 09:41:34 -0500

    Importacion de la clase JOptionPane

--- Java01.java
+++ Java01.java
@@ -4,7 +4,13 @@
 
 package com.mycompany.mavenproject2;
 
+import javax.swing.JOptionPane;
+
 
 public class Java01 {
+    
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog(null, "Que desea Realizar ?? \n 1: SUMA \n 2: RESTA \n ");
+    }
 
 }
