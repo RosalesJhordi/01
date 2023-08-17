b0c1d3ff3bc13032921df82e2a031b955beb460e
RosalesJhordi <1428060@senati.pe>
2023-08-17 09:53:46 -0500

    Cambio de planes

--- Java01.java
+++ Java01.java
@@ -4,13 +4,12 @@
 
 package com.mycompany.mavenproject2;
 
-import javax.swing.JOptionPane;
-
+import java.util.Scanner;
 
 public class Java01 {
     
     public static void main(String[] args) {
-        JOptionPane.showInputDialog(null, "Que desea Realizar ?? \n 1: SUMA \n 2: RESTA \n ");
+         Scanner cs = new Scanner(System.in);
     }
 
 }
